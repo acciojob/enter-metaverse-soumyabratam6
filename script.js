4c40820eccdeebd4134724eba0d3de1c8f4d1859
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2024-04-26 13:16:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+ document.getElementById('enterBtn').addEventListener('click', function() {
+      document.getElementById('status').innerHTML = '<h1>Entered Metaverse</h1>';
+    });
